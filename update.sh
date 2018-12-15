ce786bdf380a638793adcc0347a3eb34a338510d
OstrichBot <39704695+OstrichBot@users.noreply.github.com>
2018-12-15 14:07:35 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm /etc/pihole/regex.list
-rm /etc/pihole/adlists.list
-wget -O /etc/pihole/adlists.list https://raw.githubusercontent.com/OstrichBot/pihole/master/adlists.list
-wget -O /etc/pihole/regex.list https://raw.githubusercontent.com/OstrichBot/pihole/master/regex.list
+sudo rm /etc/pihole/regex.list
+sudo rm /etc/pihole/adlists.list
+sudo wget -O /etc/pihole/adlists.list https://raw.githubusercontent.com/OstrichBot/pihole/master/adlists.list
+sudo wget -O /etc/pihole/regex.list https://raw.githubusercontent.com/OstrichBot/pihole/master/regex.list
 pihole restartdns
