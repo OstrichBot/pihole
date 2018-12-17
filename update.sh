1c6a09dd96df07bf95fb48aee27c460c25ec120c
OstrichBot <39704695+OstrichBot@users.noreply.github.com>
2018-12-16 19:24:29 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # curl -sSL https://raw.githubusercontent.com/OstrichBot/pihole/master/update.sh | bash
 
+# Stop PiHole
+pihole disable
+
 # Remove Old Files
 rm /etc/pihole/regex.list
 rm /etc/pihole/adlists.list
@@ -8,6 +11,9 @@ rm /etc/pihole/adlists.list
 # Download New Files
 wget -O /etc/pihole/adlists.list https://raw.githubusercontent.com/OstrichBot/pihole/master/adlists.list
 wget -O /etc/pihole/regex.list https://raw.githubusercontent.com/OstrichBot/pihole/master/regex.list
+
+# Enable PiHole
+pihole enable
 
 # Restart DNS for regex filters
 pihole restartdns
