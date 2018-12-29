7034fcec80725ba6a9e723767e169595224d4269
OstrichBot <39704695+OstrichBot@users.noreply.github.com>
2018-12-29 12:06:06 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -65,7 +65,7 @@ wait
 
 # Update pihole
 # This will update gravity.list
-echo -e " $[...] \e[32m Chekcing for PiHole Updates... \e[0m"
+echo -e " [...] \e[32m Chekcing for PiHole Updates... \e[0m"
 pihole updatePihole > /dev/null
 wait
 echo -e " ${TICK} \e[32m Done... \e[0m"
