7660856fb8adcb0ef50d38c771ebaae89dc22b78
OstrichBot <39704695+OstrichBot@users.noreply.github.com>
2018-12-29 12:40:54 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bashrc
 # Based on: https://github.com/anudeepND/whitelist
 # Original License: https://github.com/anudeepND/whitelist/blob/master/LICENSE
 # License: https://github.com/OstrichBot/pihole/blob/master/LICENSE
