3c8aba595f6efe2aa9504c3f0bc2a37b8aa7d4e3
OstrichBot <39704695+OstrichBot@users.noreply.github.com>
2018-12-29 13:13:42 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -75,11 +75,11 @@ pihole -g > /dev/null
 wait
 
 # Provide stats on block lists
-echo -en "  ${TICK}\e[32m whitelist.txt entries: \e[0m"
+echo -en "  [i]\e[32m whitelist.txt entries: \e[0m"
 more /etc/pihole/whitelist.txt | sort -u | wc -l
-echo -en "  ${TICK}\e[32m gravity.list entries: \e[0m"
+echo -en "  [i]\e[32m gravity.list entries: \e[0m"
 more /etc/pihole/gravity.list | sort -u | wc -l
-echo -en "  ${TICK}\e[32m regex.list entries: \e[0m"
+echo -en "  [i]\e[32m regex.list entries: \e[0m"
 more /etc/pihole/regex.list | sort -u | wc -l
 
 # Display PiHsedole status
