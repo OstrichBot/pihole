77e4f5d82d28867cff8d345777d6fccc41fb6980
OstrichBot <39704695+OstrichBot@users.noreply.github.com>
2018-12-30 16:45:29 -0500

    Update install_amd64.sh

--- cloudflared/install_amd64.sh
+++ cloudflared/install_amd64.sh
@@ -3,19 +3,28 @@
 # REF: https://bendews.com/posts/implement-dns-over-https/
 
 # Download latest cloudflared client from source
-wget https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb
+sudo wget https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb
 sudo apt-get install ./cloudflared-stable-linux-amd64.deb
 sudo useradd -s /usr/sbin/nologin -r -M cloudflared
 
 # Download cloudflared commandline parameters
-wget -O /etc/default/cloudflared https://raw.githubusercontent.com/OstrichBot/pihole/master/cloudflared/cloudflared.arg
+sudo wget -O /etc/default/cloudflared https://raw.githubusercontent.com/OstrichBot/pihole/master/cloudflared/cloudflared.arg
 sudo chown cloudflared:cloudflared /etc/default/cloudflared
 sudo chown cloudflared:cloudflared /usr/local/bin/cloudflared
 
 # Download cloudflared.service & enable
-wget -O /lib/systemd/system/cloudflared.service https://raw.githubusercontent.com/OstrichBot/pihole/master/cloudflared/cloudflared.service
+sudo wget -O /lib/systemd/system/cloudflared.service https://raw.githubusercontent.com/OstrichBot/pihole/master/cloudflared/cloudflared.service
 sudo systemctl enable cloudflared
 sudo systemctl start cloudflared
 
 # Show that it worked
 dig @127.0.0.1 -p 5053 google.com
+
+# Remark out server entries in dnsmasq.d
+sudo sed -i 's/server=/#server=/g' /etc/dnsmasq.d/*.conf
+
+# Update insert config in dnsmasq.d that directs PiHole to use CloudFlared
+wget -O /etc/dnsmasq.d/50-cloudflared.conf https://raw.githubusercontent.com/OstrichBot/pihole/master/cloudflared/50-cloudflared.conf
+
+# Remove entries from PiHole setup variables
+sudo sed -i 's/PIHOLE_DNS/#PIHOLE_DNS/g' /etc/pihole/setupVars.conf
