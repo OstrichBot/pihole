bab492103dd60af52c6b963f7ff9a444d4838bab
OstrichBot <39704695+OstrichBot@users.noreply.github.com>
2018-12-30 23:22:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 wget -qO /usr/local/bin/pihole_update.sh https://raw.githubusercontent.com/OstrichBot/pihole/master/pihole_update.sh
 chmod +x /usr/local/bin/pihole_update.sh
-/usr/local/bin/pihole_update.sh
