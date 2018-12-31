8089dfcc3cdb5b17d15312a3debd77f4b7391b3d
OstrichBot <39704695+OstrichBot@users.noreply.github.com>
2018-12-30 22:30:12 -0500

    Update pihole_update.sh

--- pihole_update.sh
+++ pihole_update.sh
@@ -95,10 +95,18 @@ rm /etc/pihole/regex.list
 rm /etc/pihole/adlists.list
 wait
 
-# adlists.list
-echo -e "  ${TICK}\e[32m Downloading adlists.list... \e[0m"
-wget -O /etc/pihole/adlists.list https://raw.githubusercontent.com/OstrichBot/pihole/master/adlists.list > /dev/null 2>&1
-wait
+# Measure Memory
+memAvail=$(grep MemAvailable /proc/meminfo | awk '{print $2}')
+memTotal=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+
+# adlists.list // Scale on Memory
+if (($memTotal > 1000000)); then
+	echo -e "  ${TICK}\e[32m Downloading extended adlists.list... \e[0m"
+	wget -O /etc/pihole/adlists.list https://raw.githubusercontent.com/OstrichBot/pihole/master/adlists.list > /dev/null 2>&1
+else
+	echo -e "  ${TICK}\e[32m Downloading adlists.list... \e[0m"
+	wget -O /etc/pihole/adlists.list https://raw.githubusercontent.com/OstrichBot/pihole/master/adlists.list > /dev/null 2>&1
+fi
 
 # regex.list
 echo -e "  ${TICK}\e[32m Downloading regex.list... \e[0m"
