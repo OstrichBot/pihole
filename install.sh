fb7f562852e97894762e2a3f3629f6dea3b29830
OstrichBot <39704695+OstrichBot@users.noreply.github.com>
2019-07-20 12:15:21 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 wget -qO /usr/local/bin/pihole_update.sh https://raw.githubusercontent.com/OstrichBot/pihole/master/pihole_update.sh
 wget -q0 /etc/dnsmasq.d/02-dnssec.conf https://raw.githubusercontent.com/OstrichBot/pihole/master/dnsmasq.d/02-dnssec.conf
 wget -q0 /etc/dnsmasq.d/04-servers.conf https://raw.githubusercontent.com/OstrichBot/pihole/master/dnsmasq.d/04-servers.conf
+wget -q0 /etc/dnsmasq.d/03-interface.conf https://raw.githubusercontent.com/OstrichBot/pihole/master/dnsmasq.d/03-interface.conf
 chmod +x /usr/local/bin/pihole_update.sh
 /usr/local/bin/pihole_update.sh
